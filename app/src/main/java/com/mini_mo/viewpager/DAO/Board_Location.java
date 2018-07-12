2b47d6ec4070ba3698938fc91c18bd86efbb3628
frenchblack <qkfqkfdl387@naver.com>
2018-07-12 12:19:44 +0900

    //Read_Board_Location 수정

--- app/src/main/java/com/mini_mo/viewpager/DAO/Board_Location.java
+++ app/src/main/java/com/mini_mo/viewpager/DAO/Board_Location.java
@@ -1,14 +1,17 @@
 package com.mini_mo.viewpager.DAO;
 
+import java.util.ArrayList;
+
 public class Board_Location
 {
     public double latitude;
     public double longitude;
     public int board_count;
+    public ArrayList<Board_Location_List> bll;
 
     public Board_Location()
     {
-
+        bll = new ArrayList<Board_Location_List>();
     }
     public Board_Location( double lat, double lon, int count )
     {
---
+++ app/src/main/java/com/mini_mo/viewpager/DAO/Board_Location_List.java
@@ -0,0 +1,12 @@
+package com.mini_mo.viewpager.DAO;
+
+public class Board_Location_List
+{
+   public int board_num;
+   public String content;
+   public String id;
+   public String user_photo;
+   public int photo_count = 0;
+   public String board_photo = null;
+
+}
