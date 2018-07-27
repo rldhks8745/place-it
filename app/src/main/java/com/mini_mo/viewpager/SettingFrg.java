b0c5f75c3866a768f0b0bf73cd213a6328dbe056
RohHyeonMin <mini_mo@naver.com>
2018-07-27 15:09:51 +0900

    setting

--- app/src/main/java/com/mini_mo/viewpager/SettingFrg.java
+++ app/src/main/java/com/mini_mo/viewpager/SettingFrg.java
@@ -1,10 +1,24 @@
 package com.mini_mo.viewpager;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by 노현민 on 2018-07-27.
  */
 
 public class SettingFrg extends Fragment {
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.activity_setting, container, false);
+
+        return view;
+    }
 }
