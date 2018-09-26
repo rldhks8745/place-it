777c1c5d5f40e6380f0e5137ddfb45c9dc9de89d
frenchblack <qkfqkfdl387@naver.com>
2018-09-26 19:55:58 +0900

    //서버 번경

--- app/src/main/java/com/mini_mo/viewpager/DAO/HTTPURL.java
+++ app/src/main/java/com/mini_mo/viewpager/DAO/HTTPURL.java
@@ -2,7 +2,7 @@ package com.mini_mo.viewpager.DAO;
 
 public class HTTPURL
 {
-   public String addrs = "http://13.209.99.85:8080/";
+   public String addrs = "http://104.198.86.112:8080/";
     //public String addrs = "http://175.118.234.215:8080/";
     public String jsourl= "data2.jsp";
     public String imageurl = "data.jsp";
